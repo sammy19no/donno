120af53d7c28dda70481d8498f4a6fc9ef3935cc
sammy19no <47357112+sammy19no@users.noreply.github.com>
2019-02-05 09:10:59 -0500

    Update save.js

--- scripts/save.js
+++ scripts/save.js
@@ -117,5 +117,5 @@ function save() {
 	
 	var filename = "candybox_" + year.substring(2, 4) + month + day + "_" + hour + "-" + min;
 	var blob = new Blob([save_text], {type: "text/plain;charset=utf-8"});
-	saveAs(blob, filename+".cs");
+	saveAs(blob, filename+".txt");
 }
